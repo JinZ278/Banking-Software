85813b8ecb5092ba82de69909d5a0d713da9bc4f
Jin <jz679@drexel.edu>
2021-05-15 22:43:25 -0400

    Final review

--- src/test/java/CommandProcessorTest.java
+++ src/test/java/CommandProcessorTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CommandProcessorTest {
 
+    public final String VALID_CREATE_COMMAND = "Create Checking 12345678 1";
     public final String VALID_CREATE_CHECKING_COMMAND = "Create Checking 12345678 1.5";
     public final String VALID_CREATE_SAVINGS_COMMAND = "Create Savings 29292929 3.0";
     public final String VALID_CREATE_CD_COMMAND = "Create Cd 99999999 5.7 1000";
@@ -25,28 +26,29 @@ public class CommandProcessorTest {
     }
 
     @Test
-    public void process_create_checking_command() {
+    public void processor_creates_account() {
         assertTrue(bank.getAccounts().get("12345678") == null);
-        commandProcessor.process(VALID_CREATE_CHECKING_COMMAND);
+        commandProcessor.process(VALID_CREATE_COMMAND);
         assertTrue(bank.getAccounts().get("12345678") != null);
+    }
+
+    @Test
+    public void process_create_checking_command() {
+        commandProcessor.process(VALID_CREATE_CHECKING_COMMAND);
         assertTrue(bank.getAccounts().get("12345678").getApr() == 1.5);
         assertTrue(bank.getAccounts().get("12345678").getBalance() == 0);
     }
 
     @Test
     public void process_create_savings_command() {
-        assertTrue(bank.getAccounts().get("29292929") == null);
         commandProcessor.process(VALID_CREATE_SAVINGS_COMMAND);
-        assertTrue(bank.getAccounts().get("29292929") != null);
         assertTrue(bank.getAccounts().get("29292929").getApr() == 3.0);
         assertTrue(bank.getAccounts().get("29292929").getBalance() == 0);
     }
 
     @Test
     public void process_create_cd_command() {
-        assertTrue(bank.getAccounts().get("99999999") == null);
         commandProcessor.process(VALID_CREATE_CD_COMMAND);
-        assertTrue(bank.getAccounts().get("99999999") != null);
         assertTrue(bank.getAccounts().get("99999999").getApr() == 5.7);
         assertTrue(bank.getAccounts().get("99999999").getBalance() == 1000);
     }
@@ -71,8 +73,10 @@ public class CommandProcessorTest {
     public void process_deposit_twice() {
         commandProcessor.process(VALID_CREATE_CHECKING_COMMAND);
         assertTrue(bank.getAccounts().get("12345678").getBalance() == 0);
+
         commandProcessor.process("Deposit 12345678 2000");
         assertTrue(bank.getAccounts().get("12345678").getBalance() == 2000);
+
         commandProcessor.process("Deposit 12345678 69.42");
         assertTrue(bank.getAccounts().get("12345678").getBalance() == 2069.42);
     }
