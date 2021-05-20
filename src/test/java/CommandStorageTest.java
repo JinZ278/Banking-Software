aaf49ef315262144296c4dd8715d460b9612e8b3
Jin <jz679@drexel.edu>
2021-05-20 17:18:57 -0400

    Created Create and Deposit Processor

--- src/test/java/CommandStorageTest.java
+++ src/test/java/CommandStorageTest.java
@@ -12,13 +12,19 @@ public class CommandStorageTest {
     public final String INVALID_STRING = "efjbjfaowfaoawfjwaslakjbf auwhoabwf auwf";
     public final String ANOTHER_STRING = "huekkbefsfhsfnd";
 
-    public List<String> list1 = new ArrayList<>();
+    List<String> list1;
 
     CommandStorage commandStorage;
 
     @BeforeEach
     void setup() {
+        list1 = new ArrayList<>();
         commandStorage = new CommandStorage();
+    }
+
+    @Test
+    public void initially_empty_list() {
+        assertEquals(list1, commandStorage.invalidStrings);
     }
 
     @Test
@@ -34,6 +40,11 @@ public class CommandStorageTest {
         list1.add(INVALID_STRING);
         list1.add(ANOTHER_STRING);
         assertEquals(list1, commandStorage.getInvalidStrings());
+    }
+
+    @Test
+    public void returning_all_strings() {
+        
     }
 
 }
