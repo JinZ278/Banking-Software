28b5732c1e6f18bf84162581901f1cb53047a41c
Jin <jz679@drexel.edu>
2021-06-10 18:43:28 -0400

    Transfer Processor Done

--- src/main/java/banking/TransferProcessor.java
+++ src/main/java/banking/TransferProcessor.java
@@ -2,4 +2,15 @@ package banking;
 
 public class TransferProcessor {
 
+    protected Bank bank;
+
+    TransferProcessor(Bank bank) {
+        this.bank = bank;
+    }
+
+    public void processTransfer(String[] info) {
+        double value = Double.parseDouble(info[3]);
+        this.bank.getAccounts().get(info[1]).withdraw(value);
+        this.bank.getAccounts().get(info[2]).deposit(value);
+    }
 }
